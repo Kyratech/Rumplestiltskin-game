842e0117d14db8905ffb4bb01723bf220e7c4e28
Matthew Langford <ml2g14@soton.ac.uk>
2017-03-24 12:46:43 +0000

    Added warp prefab that loads scenes upon collision

---
+++ Assets/Scripts/SceneWarp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneWarp : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<PlayerMovement>().warpCooldown <= 0)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
