350df37a3721571afbd54a1e92a0624fe5a577d1
Mattadon <ml2g14@soton.ac.uk>
2017-04-10 18:32:32 +0100

    Started work on new conversation engine

---
+++ Assets/Scripts/DialogItemController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Dialog items are either text boxes or dialog option lists
+ * They should advance through the contents of the item,
+ * and then activate the next item in the chain.
+ */
+public abstract class DialogItemController : MonoBehaviour
+{
+    //The gameobject that displays this dialog item
+    [SerializeField]
+    private GameObject myDialog;
+    //The gameobject of the next item in the chain
+    [SerializeField]
+    protected GameObject nextDialog;
+
+    /*
+     * Show the next line of dialog
+     * or advance to the next dialog object
+     */
+    public abstract void showNext();
+}
--- Assets/Scripts/DialogOptionController.cs
+++ Assets/Scripts/DialogOptionController.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public abstract class DialogOptionController : MonoBehaviour
+public abstract class DialogOptionController : DialogItemController
 {
     //Dialog Options
     private List<GameObject> options;
