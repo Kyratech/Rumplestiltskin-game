9ff9e8b4cf73f28b1155553d5ed4f36babec392d
Mattadon <ml2g14@soton.ac.uk>
2017-04-12 19:22:22 +0100

    Added inventory screen, toggled by ESC
    Currently shows book whether it is picked
    up or not.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Persists throughout the course of the game.
+ * Keeps track of inventory
+ * and narrative engine progress
+ * Eg: Which characters spoken to + which dialog option, etc.
+ */
+public class GameController : MonoBehaviour
+{
+    //Inventory
+    private bool hasBook;
+
+    public bool HasBook { get; set; }
+        
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        hasBook = false;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour {
     private LayerMask interactiveLayerMask = -1;
     //Is the player currently interacting? If so, disable movement.
     private bool interacting;
+    //Is the game paused? If so, disable all input
+    private bool paused;
 
     //What direction is the player facing?
     private enum directions
@@ -21,6 +23,8 @@ public class PlayerMovement : MonoBehaviour {
     private directions lastDirection;
 
     public float warpCooldown;
+
+    private GameObject inventoryScreen;
 
     Rigidbody2D rb;
 	Animator anim;
@@ -32,30 +36,45 @@ public class PlayerMovement : MonoBehaviour {
 
 		warpCooldown = 0;
         interacting = false;
+        paused = false;
 
         lastDirection = directions.down;
+
+        inventoryScreen = GameObject.Find("Inventory_Prefab");
+        inventoryScreen.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(warpCooldown > 0) 
-			warpCooldown -= Time.deltaTime;
 
-		Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //Use esc to bring up the inventory screen
+        if (Input.GetButtonDown("Escape"))
+        {
+            inventoryScreen.SetActive(!inventoryScreen.activeInHierarchy);
+            paused = inventoryScreen.activeInHierarchy;
+        }
 
-		if(movementVector != Vector2.zero && !interacting){
-			anim.SetBool("isWalking", true);
-			anim.SetFloat("input_x", movementVector.x);
-			anim.SetFloat("input_y", movementVector.y);
+        if (warpCooldown > 0)
+            warpCooldown -= Time.deltaTime;
+
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movementVector != Vector2.zero && !interacting && !paused)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("input_x", movementVector.x);
+            anim.SetFloat("input_y", movementVector.y);
 
             //Find the direction
-            if(Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y)){
+            if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
+            {
                 if (movementVector.x > 0)
                     lastDirection = directions.right;
                 else
                     lastDirection = directions.left;
             }
-            else{
+            else
+            {
                 if (movementVector.y > 0)
                     lastDirection = directions.up;
                 else
@@ -63,43 +82,52 @@ public class PlayerMovement : MonoBehaviour {
             }
 
             rb.MovePosition(rb.position + movementVector * moveSpeed * Time.deltaTime);
-        } else {
-			anim.SetBool("isWalking", false);
-		}
-
-        //Ok, the enum probably isn't the simplest way of doing this,
-        //But it is at least extensible
-        Vector2 inspectRayDirection;
-        switch (lastDirection)
+        }
+        else
         {
-            case directions.right:
-                inspectRayDirection = new Vector2(1, 0);
-                break;
-            case directions.left:
-                inspectRayDirection = new Vector2(-1, 0);
-                break;
-            case directions.up:
-                inspectRayDirection = new Vector2(0, 1);
-                break;
-            default:
-            case directions.down:
-                inspectRayDirection = new Vector2(0, -1);
-                break;
+            anim.SetBool("isWalking", false);
         }
 
-        //Use raycasts to determine if the player is in front of something interactable
-        RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, inspectRayDirection, 1.5f, interactiveLayerMask);
-        Debug.DrawRay(rb.position, inspectRayDirection, Color.green);
-        if (raycastHit.collider != null)
+        if (!paused)
         {
-            if(Input.GetButtonDown("Inspect"))
+
+            //Ok, the enum probably isn't the simplest way of doing this,
+            //But it is at least extensible
+            Vector2 inspectRayDirection;
+            switch (lastDirection)
             {
-                GameObject other = raycastHit.collider.gameObject;
-                if(other.tag.Equals("Door")) {
-                	other.GetComponent<DoorWarp>().ExitRoom();
-                } else {
-                	//Toggle interacting state
-                	interacting = other.GetComponent<InspectTile>().toggleDialog();
+                case directions.right:
+                    inspectRayDirection = new Vector2(1, 0);
+                    break;
+                case directions.left:
+                    inspectRayDirection = new Vector2(-1, 0);
+                    break;
+                case directions.up:
+                    inspectRayDirection = new Vector2(0, 1);
+                    break;
+                default:
+                case directions.down:
+                    inspectRayDirection = new Vector2(0, -1);
+                    break;
+            }
+
+            //Use raycasts to determine if the player is in front of something interactable
+            RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, inspectRayDirection, 1.5f, interactiveLayerMask);
+            Debug.DrawRay(rb.position, inspectRayDirection, Color.green);
+            if (raycastHit.collider != null)
+            {
+                if (Input.GetButtonDown("Inspect"))
+                {
+                    GameObject other = raycastHit.collider.gameObject;
+                    if (other.tag.Equals("Door"))
+                    {
+                        other.GetComponent<DoorWarp>().ExitRoom();
+                    }
+                    else
+                    {
+                        //Toggle interacting state
+                        interacting = other.GetComponent<InspectTile>().toggleDialog();
+                    }
                 }
             }
         }
