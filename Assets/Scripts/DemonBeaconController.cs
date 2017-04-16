843286c9056313b1e52f6076da841ece23081519
Mattadon <ml2g14@soton.ac.uk>
2017-04-16 22:36:10 +0100

    Added toggleable beacons to demon hub (for puzzle)

---
+++ Assets/Scripts/DemonBeaconController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonBeaconController : InspectTile
+{
+    [SerializeField]
+    private Sprite lightSprite;
+    [SerializeField]
+    private Sprite darkSprite;
+    [SerializeField]
+    private bool activated;
+
+    public override bool toggleDialog()
+    {
+        activated = !activated;
+        if (activated)
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = lightSprite;
+        else
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = darkSprite;
+
+        //Don't lock the player into an interaction
+        return false;
+    }
+}
