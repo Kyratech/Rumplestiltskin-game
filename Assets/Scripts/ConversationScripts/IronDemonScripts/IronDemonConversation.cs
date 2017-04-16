8e366282b1dbdf488ea7d8822f79f62929c2922a
Mattadon <ml2g14@soton.ac.uk>
2017-04-16 22:54:26 +0100

    Started work on Iron demon conversation

---
+++ Assets/Scripts/ConversationScripts/IronDemonScripts/IronDemonConversation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IronDemonConversation : ConversationTile
+{
+    [SerializeField]
+    private Sprite ironIdle;
+    [SerializeField]
+    private Sprite ironTalk;
+    [SerializeField]
+    private GameObject ironDemon;
+
+    public override void initText()
+    {
+        
+    }
+
+    public override void dialogStart()
+    {
+        ironDemon.GetComponent<SpriteRenderer>().sprite = ironTalk;
+    }
+
+    public override void dialogFinish()
+    {
+        ironDemon.GetComponent<SpriteRenderer>().sprite = ironIdle;
+    }
+}
