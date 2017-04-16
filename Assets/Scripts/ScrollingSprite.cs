c932edcac149e422afeaed8553e08a3c0bd06071
Mattadon <ml2g14@soton.ac.uk>
2017-04-16 17:30:47 +0100

    Added scrolling mist to the top of demon hub

---
+++ Assets/Scripts/ScrollingSprite.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Make an object scroll in a direction,
+ * looping after travelling a certain distance
+ */
+public class ScrollingSprite : MonoBehaviour
+{
+    //How far the object crolls before looping
+    [SerializeField]
+    private float loopDistance;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector3 direction;
+
+    //How far the object has scrolled so far
+    private Vector3 displacement;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        direction.Normalize();
+    }
+
+    void Update()
+    {
+        displacement += direction * speed * Time.deltaTime;
+
+        if(displacement.sqrMagnitude > loopDistance * loopDistance)
+        {
+            displacement -= direction * loopDistance;
+        }
+
+        transform.position = startPos + displacement;
+    }
+}
