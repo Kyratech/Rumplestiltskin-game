f98a52f067dc3563ac9a3b27575a10de4849396f
Mattadon <ml2g14@soton.ac.uk>
2017-04-17 21:16:13 +0100

    Completing the demon beacon puzzle now raises the bridge

--- Assets/Scripts/DemonBeaconController.cs
+++ Assets/Scripts/DemonBeaconController.cs
@@ -9,6 +9,18 @@ public class DemonBeaconController : InspectTile
     private Sprite darkSprite;
     [SerializeField]
     private bool activated;
+    [SerializeField]
+    private bool targetActivated;
+
+    private DemonHub_Controller hubController;
+
+    void Start()
+    {
+        pauseTimer = 0.0f;
+
+        GameObject controllerObj = GameObject.Find("DemonHub_Controller");
+        hubController = controllerObj.GetComponent<DemonHub_Controller>();
+    }
 
     public override bool toggleDialog()
     {
@@ -18,7 +30,22 @@ public class DemonBeaconController : InspectTile
         else
             transform.gameObject.GetComponent<SpriteRenderer>().sprite = darkSprite;
 
+        hubController.updateBeacons();
+
         //Don't lock the player into an interaction
         return false;
+    }
+
+    public bool getCorrect()
+    {
+        return (activated == targetActivated);
+    }
+
+    public void setCorrect()
+    {
+        if (targetActivated)
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = lightSprite;
+        else
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = darkSprite;
     }
 }
---
+++ Assets/Scripts/DemonHub_Controller.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonHub_Controller : MonoBehaviour
+{
+    //Deal with the beacon puzzle
+    [SerializeField]
+    private GameObject northBlocker;
+    private GameObject[] beacons;
+    private bool beaconsCorrect;
+    private GameObject[] platforms;
+    private Vector3[] platformStarts;   //Position where platforms start
+    private Vector3[] platformTargets;  //Position where platforms should move to
+    private float platformMoveTimer;
+    private float platformMoveDuration = 2.0f;
+
+    void Start()
+    {
+        beacons = GameObject.FindGameObjectsWithTag("DemonBeacon");
+
+        beaconsCorrect = false;
+
+        platforms = GameObject.FindGameObjectsWithTag("DemonPlatform");
+        platformStarts = new Vector3[platforms.Length];
+        platformTargets = new Vector3[platforms.Length];
+        for(int i = 0; i < platforms.Length; i++)
+        {
+            platformStarts[i] = platforms[i].transform.position;
+            platformTargets[i] = platforms[i].transform.position + new Vector3(0.0f, 7.0f, 0.0f);
+            //Debug.Log("Platform at: " + platformStarts[i] + " has target at:" + platformTargets[i]);
+        }
+
+        platformMoveTimer = 0;
+    }
+
+    void Update()
+    {
+        if(beaconsCorrect && platformMoveTimer < platformMoveDuration)
+        {
+            float moveFraction = platformMoveTimer / platformMoveDuration;
+
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                FloatAnimation platformController = platforms[i].GetComponent<FloatAnimation>();
+                platformController.setInitialPosition(Vector3.Lerp(platformStarts[i], platformTargets[i], moveFraction));
+            }
+
+            platformMoveTimer += Time.deltaTime;
+        }
+    }
+
+    public void updateBeacons()
+    {
+        bool allCorrect = true;
+        foreach(GameObject beacon in beacons)
+        {
+            DemonBeaconController beaconController = beacon.GetComponent<DemonBeaconController>();
+            if(!beaconController.getCorrect())
+            {
+                allCorrect = false;
+                break;
+            }
+        }
+
+        if(allCorrect)
+        {
+            Debug.Log("All correct");
+            beaconsCorrect = true;
+
+            northBlocker.SetActive(false);
+        }
+    }
+}
