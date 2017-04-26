6849df75cda68d967b4dd5a8ca57b9a91f4fc8b6
Samuel Beresford <seb1g13@soton.ac.uk>
2017-04-26 19:49:42 +0100

    Added locked door dialog to Temple and tidied up inspect tile sizes

--- Assets/Scripts/LockedDoorWarp.cs
+++ Assets/Scripts/LockedDoorWarp.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class LockedDoorWarp : SceneWarp {
 
+	public InspectTile lockedInspect;
+
 	public void ExitRoom() {
 		if (controller.hasKey) {
         	controller.warpID = warpID;
 			SceneManager.LoadScene(sceneToLoad);
+		}
+		else {
+			lockedInspect.toggleDialog();
 		}
 	}
 
