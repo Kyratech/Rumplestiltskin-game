2baabf189f5fc51093247d9aaab269c7f44ef69b
Samuel Beresford <seb1g13@soton.ac.uk>
2017-04-27 19:33:56 +0100

    Added Background Music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	AudioSource audioSource;
+	public AudioClip[] audioClips;
+	public string[] scenesToChange;
+	public int audioPlaying = 0;
+
+	static bool AudioBegin = false; 
+ 	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		if (!AudioBegin) {
+			audioSource.Play();
+			DontDestroyOnLoad (transform.gameObject);
+			AudioBegin = true;
+		} 
+	}
+	void Update () {
+    	for(int i = 1; i<scenesToChange.Length+1; i++) {
+			if(audioPlaying != i && SceneManager.GetActiveScene().name == scenesToChange[i-1] && audioClips[i-1]) {
+				audioSource.Stop();
+    			audioSource.clip = audioClips[i-1];
+    			audioPlaying = i;
+    			audioSource.Play();
+    		}
+ 		}
+	}
+}
