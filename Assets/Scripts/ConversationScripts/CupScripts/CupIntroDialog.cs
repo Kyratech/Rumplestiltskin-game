2bd938cb2f2166e7a610f481d0df2d53ae25fa9a
Samuel Beresford <seb1g13@soton.ac.uk>
2017-04-27 13:41:37 +0100

    Fixed Merchant Option and added demonNAmes to cups

--- Assets/Scripts/ConversationScripts/CupScripts/CupIntroDialog.cs
+++ Assets/Scripts/ConversationScripts/CupScripts/CupIntroDialog.cs
@@ -13,26 +13,33 @@ public class CupIntroDialog : DialogTextController
     {	
     	string action = "Take";
     	string itemName;
+    	string demonName;
     	switch(contents) {
 		case Contents.Copper:
     	 	itemName = "pile of copper coins";
+    	 	demonName = "Beezlebub";
     	 	break;
 		case Contents.Iron:
-    	 	itemName = "shard of iron";
+			itemName = "shard of iron";
+    	 	demonName = "Belphegor";
     	 	break;
 		case Contents.Mercury:
-    	 	itemName = "bowl of quicksilver";
+			itemName = "bowl of quicksilver";
+    	 	demonName = "Mammon";
     	 	break;
     	 case Contents.Key:
     	 	action = "Flip";
-    	 	itemName = "switch";
+			itemName = "switch";
+    	 	demonName = "Sathanus";
     	 	break;
 		default:
     	 	itemName = "ERROR";
+    	 	demonName = "ERROR";
     	 	break;
     	}
         List<string> lines = new List<string>();
         lines.Add("What's this?");
+        lines.Add("An offering to " + demonName);
         lines.Add("Hmm... Looks like a " + itemName);
         lines.Add("(" + action + " the " + itemName + "?)");
 
